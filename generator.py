1d434ff49365ec9182f1114bd0d091945a39660b
LonneQuent <lonnequentin@gmail.com>
2024-01-17 11:49:47 +0100

    creation_prog

---
+++ generator.py
@@ -0,0 +1,26 @@
+## Générateur de poèmes basé sur les sagas
+
+import cohere
+import tiktoken
+import openai
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key='votre_clé_API')
+
+choix_film_user = None
+perso_user = None
+choix_poem_user = None
+
+#On utilise l'API de ChatGPT comme générateur de poème en lui donnant les instructions sélectionnées précédemment.
+chat_completion = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "tu es "+perso_user+" de l'univers de "+choix_film_user},
+        {"role": "user","content": "fais moi un poème de type "+choix_poem_user+" en te comportant comme "+perso_user},
+    ],
+    model="gpt-3.5-turbo",
+)
+
+#On affiche le poème.
+print("\n Voici votre poème:\n")
+print(chat_completion.choices[0].message.content)
